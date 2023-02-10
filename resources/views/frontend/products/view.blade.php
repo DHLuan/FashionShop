c1b86d8a6f995e4f645cff9b6c3155fde254399e
DHLuan <dhl13052001@gmail.com>
2023-02-11 03:14:52 +0700

    Changes

--- resources/views/frontend/products/view.blade.php
+++ resources/views/frontend/products/view.blade.php
@@ -42,9 +42,9 @@
                             <div class="col-md-2">
                                 <label for="Quantity">Quantity</label>
                                 <div class="input-group text-center mb-3">
-                                    <span class="input-group-text">-</span>
-                                    <input type="text" name="quantity" value="1" class="form-control"/>
-                                    <span class="input-group-text">+</span>
+                                    <button class="input-group-text decrement-btn">-</button>
+                                    <input type="text" name="quantity " value="1" class="form-control qty-input text-center"/>
+                                    <button class="input-group-text increment-btn">+</button>
                                 </div>
                             </div>
                             <div class="col-md-10">
@@ -66,4 +66,36 @@
         </div>
     </div>
 
+@endsection
+
+@section('scripts')
+    <script>
+        $(document).ready(function () {
+            $('.increment-btn').click(function (e) {
+                e.preventDefault();
+
+                var inc_value = $('.qty-input').val();
+                var value = parseInt(inc_value, 10);
+                value = isNaN(value) ? 0 : value;
+                if(value < 10)
+                {
+                    value ++;
+                    $('.qty-input').val(value);
+                }
+            });
+
+            $('.decrement-btn').click(function (e) {
+                e.preventDefault();
+
+                var dec_value = $('.qty-input').val();
+                var value = parseInt(dec_value, 10);
+                value = isNaN(value) ? 0 : value;
+                if(value > 1)
+                {
+                    value --;
+                    $('.qty-input').val(value);
+                }
+            });
+        });
+    </script>
 @endsection
